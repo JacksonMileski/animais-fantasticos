04884a52caf0d4909814a3a44ad6dfbcd2649e04
Jackson Mileski <jacksonktrabalho@gmail.com>
2023-10-23 20:03:55 -0300

    Scroll transformado em Class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initScrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import tabnav from './modules/tabnav.js';
@@ -11,7 +11,24 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
-initScrollSuave();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init(); // FIXME: para iniciar
+/* FIXME: o ideal é que pegamos cada função daqui e possa possa colocar
+em qualquer site, e atráves de uma configuração ou outra ela possa usar
+FIXME: vou começar com o scrollsuave, fiz um git checkout -b refatorar-scrollsuave
+entao agora estou no meu novo branch que é o refatorar-scrollsuave, e agora
+vou fazer algumas coisas no initScrollSuave, mas primeiro eu dou um npm run dev
+para ficar rodando o modo de desenvolvimento, depois de eu fazer as coisas no scroll-suave,
+no caso transformei em class para o usuario poder usar esse arquivo de modo que ele possa
+usar para seus projetos, vou no import e mudo para ScrollSuave, e agora faço como fosse o usuario
+colocando as opçoes no newScrollSuave, no segundo argumento eu posso fazer uma constante e passar ela
+por exemplo const options = {
+    behavior: 'smooth',
+    block: 'start',
+}
+e passo ela no segundo argumento
+
+agora q terminei tudo é legal que a pessoa q usar isso aqui vai poder importar para qualquer site praticamente */
 initAnimacaoScroll();
 initAccordion();
 tabnav();
