4d683949f1ecc555b3d5cfcafc429462459c25d2
Jackson Mileski <jacksonktrabalho@gmail.com>
2023-10-23 18:04:41 -0300

    adicionado package-lock.json

--- js/script.js
+++ js/script.js
@@ -24,16 +24,3 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
-
-import $ from 'jquery'; //agora posso colocar qual o nome da funçao que eu quero, entao usei o $ de jquery
-//from 'jquery' no caso como estou usando node.js entao nem preciso colocar o caminho do jquery, ele vai
-//achar automaticamente no meu node_modules o arquivo do jquery. pelo o que entendi do lado de import
-// é só o nome que eu dei, n precisava ser '$'
-import _ from 'lodash';
-
-//FIXME: função do jquery $('nav').hide(); esconde a nav
-
-//lodash me dá métodos diferentes para arrays, nesse .difference ele me da o que eu tenho na primeira array q eu
-//n tenho na segunda array, a resposta vai me dar uma array com uva
-const diferenca = _.difference(['Banana', 'Morango', 'Uva'], ['Banana', 'Morango', 'Pêra']);
-console.log(diferenca)
