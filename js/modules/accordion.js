e24d5ceb2634976f299e05ed0aa2a27bbee0b65b
Jackson Mileski <jacksonktrabalho@gmail.com>
2023-10-23 21:53:59 -0300

    Accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -28,4 +28,5 @@ export default class initAccordion { // FIXME: transformando em uma class
 }
 // FIXME: agora so dar um git add -A git commit -a -m 'Accordion transformado em classe', depois dou um git checkout main
 // e git pull para ver se nao teve diferença nenhuma no codigo, caso n teve eu volto pro meu git checkout refatorar-accordion
-//no caso o git pull baixa no branch que eu estiver as atualizações pelo o que entendi
+//no caso o git pull baixa no branch que eu estiver as atualizações pelo o que entendi, agora dou o git merge main, n vai ter nenhuma
+//mudança, e depois dou o git push, e copio e colo o codigo
